30e6d232f372dbbab50e54419c24d77e76194755
Troy <tydamon@hotmail.com>
2024-11-18 21:11:32 +1100

    styling

--- client/src/pages/Calender.jsx
+++ client/src/pages/Calender.jsx
@@ -3,9 +3,7 @@ import Cal from '../components/Cal';
 export default function Calender() {
   return (
     <div className="contact-container">
-      <h1>Calender</h1>
-
-      <div className="separator-line"></div>
+      <h1>Jam Calender</h1>
 
       <div className="separator-line"></div>
 
@@ -13,8 +11,15 @@ export default function Calender() {
 
         <h3>Instructions</h3>
         <h5>Click on your name to change availability</h5>
-        <h5>Red = Unavailable</h5>
-        <h5>Green = Available</h5>
+
+<div className='color-box'>
+
+        <p className='red-text'>Red = Unavailable</p>
+        <p className='green-text'>Green = Available</p>
+        <p className='white-text'>White = Undecided</p>
+        
+</div>
+
 
         <Cal />
 
