bc123067783ab74b3f5fbe63b798185534e158b1
Troy <tydamon@hotmail.com>
2024-12-12 02:32:50 +1100

    css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,9 +3,9 @@ function Header() {
     <header className='header'>
       <header className='header-gold'>
         <div className="wrap">
-          <p className='the'>
-            <span className='word'>Ultimate </span> 
-            <span className="glitch" data-text="Horror">Band</span> 
+          <p className='the'> Your
+            <span className='word'> Band </span> 
+            <span className="glitch" data-text="Horror">Name</span> 
           </p>
         </div>
 
