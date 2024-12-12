cd985efd1437cb0af82fef5e52dd4a1bc6d4b9d2
Troy <tydamon@hotmail.com>
2024-12-13 00:01:08 +1100

    ...

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,12 @@
 function Header() {
   return (
     <header className='header'>
-      <header className='header-gold'>
         <div className="wrap">
           <p className='the'> Your
             <span className='word'> Band </span> 
             <span className="glitch" data-text="Horror">Name</span> 
           </p>
         </div>
-      </header>
     </header>
   );
 }
