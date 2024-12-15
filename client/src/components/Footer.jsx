5cb2c360fe5e5858332f4c83c09e084eb964af4e
Troy <tydamon@hotmail.com>
2024-12-15 18:11:09 +1100

    added link in footer again, removed right: 0 from music-player-container so it doesn't cover full screen and block footer links

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 function Footer() {
   return (
     <footer className="text-center text-white">
@@ -5,6 +8,15 @@ function Footer() {
         <div className="col-lg-8">
 
           <p className="footer-text">sifzerda 🎵 2024</p>
+
+
+          <a href="https://github.com/sifzerda/bandpage" target="_blank" rel="noopener noreferrer">
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
+
+          <br></br>
+
+          <a href="https://react-td-portfolio.netlify.app/" target="_blank" rel="noopener noreferrer">My Project Portfolio</a>
 
         </div>
       </div>
